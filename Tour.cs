484cc57d387b511d19e0059069e72a281bb3dcc6
asmanov <asmanov.a@gmail.com>
2023-05-23 21:41:59 +0300

    Add project files.

---
+++ Tour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency
+{
+    public class Tour
+    {
+        public Tour(string hotel, int stars, string destination, string whereFrom, double cost, int amountOfDay, DateTime date)
+        {
+            Hotel = hotel ?? throw new ArgumentNullException(nameof(hotel));
+            Stars = stars;
+            Destination = destination ?? throw new ArgumentNullException(nameof(destination));
+            WhereFrom = whereFrom ?? throw new ArgumentNullException(nameof(whereFrom));
+            Cost = cost;
+            AmountOfDay = amountOfDay;
+            Date = date;
+        }
+
+        public string Hotel { get; set; }
+        public int Stars { get; set; }
+        public string Destination { get; set; }
+        public string WhereFrom { get; set; }
+        public double Cost { get; set; }
+        public int AmountOfDay { get; set; }
+        DateTime Date { get; set; }
+
+    }
+}
